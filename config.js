55fb6b215da8ebe67e8da24fdf8b683aedd806cb
FahimCreatior <153365037+FahimCreatior@users.noreply.github.com>
2025-07-20 07:36:27 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const config = {
     TMDB_API_KEY: 'd87e27aa77fbcfe8fbebfe5f40f4f215',
         YOUTUBE_API_KEY: 'AIzaSyB9jvwntny6UBr8GAnmWLoGi9NdjPQpUeI',
-            VIMEO_ACCESS_TOKEN: 'f57a8f54d0dd15e89383d1edb627c21e'
+            VIMEO_ACCESS_TOKEN: 'nSoOGlr/sExSYlFb9LQdGQArXn2G4Z22qyiTCMHppZbBrHHBQt3FCJAt10oZSNZ+IOZ1QB1q0CvATVFt87kTnpXdqQ8w8unI39vZ9u7oX58gLIqH9zbx5oLN+ch6oqDQ'
             };
 
             export default config;
